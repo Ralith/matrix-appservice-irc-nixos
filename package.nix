1f3b6e777acb8a110d7265e7b0a2b94501b38d37
Ruben Maher <r@rkm.id.au>
2018-01-06 15:18:10 +1030

    Build package with node2nix, bump version

--- package.nix
+++ package.nix
@@ -1,26 +1,9 @@
-{ stdenv, fetchurl, fetchFromGitHub, nodejs, git, python, icu }:
-stdenv.mkDerivation rec {
-  name = "matrix-appservice-irc-${version}";
-  version = "0.7.1";
+{ stdenv, system, fetchurl, fetchgit, nodejs, python2, utillinux, runCommand,
+  writeTextFile }:
 
-  src = fetchFromGitHub {
-    owner = "matrix-org";
-    repo = "matrix-appservice-irc";
-    rev = "${version}";
-    sha256 = "0ww5ss0782ha0ry7l1h5sq1d2i3cldgqxmly6iyzd584gl3mjjv0";
+(import ./node {
+  pkgs = {
+    inherit stdenv python2 utillinux runCommand writeTextFile fetchurl fetchgit;
   };
-
-  buildInputs = [ nodejs git python icu ];
-
-  buildPhase = ''
-    export HOME=$TEMP
-    npm install
-  '';
-
-  installPhase = ''
-    mkdir -p "$out/lib"
-    mv "$PWD" "$out/lib/matrix-appservice-irc"
-    mkdir -p "$out/bin"
-    ln -s "$out/lib/matrix-appservice-irc/bin/matrix-appservice-irc" "$out/bin/"
-  '';
-}
+  inherit system nodejs;
+})."matrix-appservice-irc-0.8.0"
