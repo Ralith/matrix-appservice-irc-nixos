2cccfe271cdbc6e4bb64fb01a95929e65dd52a74
Ruben Maher <ruben@maher.fyi>
2018-08-10 21:35:39 +0930

    0.8.0 -> 0.10.1

--- node/generate.sh
+++ node/generate.sh
@@ -3,7 +3,7 @@
 rm -f default.nix node-packages.nix node-env.nix
 "$(nix-build ./node2nix.nix --no-out-link)/bin/node2nix" \
   --production \
-  --nodejs-6 \
+  --nodejs-10 \
   --input node-packages.json \
   --composition default.nix \
   --node-env node-env.nix
